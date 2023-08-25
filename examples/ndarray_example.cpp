d53c00554533cffff88898132e787f2acca1753b
Nils Wentzell <nwentzell@flatironinstitute.org>
2023-08-25 13:34:31 -0400

    Cleanup ndarray example and synchronize with paper

--- examples/ndarray_example.cpp
+++ examples/ndarray_example.cpp
@@ -1,36 +1,36 @@
-
-#include "triqs_Nevanlinna/kernels.hpp"
+#include <triqs_Nevanlinna/kernels.hpp>
 #include <mpi/mpi.hpp>
-#include <nda/traits.hpp>
+#include <nda/nda.hpp>
 #include <nda/h5.hpp>
 
 using namespace std::literals;
 
 int main(int argc, char **argv) {
+  // Initialize MPI environment
   mpi::environment env(argc, argv);
+
+  // Create kernel object
   triqs_Nevanlinna::Nevanlinna_kernel kernel;
 
-  size_t N_omega   = 5000;
-  double omega_min = -10;
-  double omega_max = 10;
-  double eta       = 0.1;
-  nda::array<std::complex<double>, 3> G_iw;
-  nda::array<std::complex<double>, 1> mesh;
+  // Define real-frequency grid
+  size_t N_w   = 5000;
+  double w_min = -10., w_max = 10.;
+  double eta = 0.1;
+  auto del   = (w_max - w_min) / (N_w - 1);
+  auto grid  = nda::basic_array{w_min + nda::arange(N_w) * del + eta * 1i};
 
-  h5::file input(std::string(DATA_PATH) + "/input.h5", 'r');
-  h5::group top(input);
-  nda::h5_read(top, std::string("data"), G_iw);
-  nda::array<double, 1> tmp;
-  nda::h5_read(top, std::string("mesh"), tmp);
-  mesh = tmp;
-  mesh *= 1i;
+  // Define imaginary time grid and data arrays
+  auto mesh = nda::array<std::complex<double>, 1>{};
+  auto G_iw = nda::array<std::complex<double>, 3>{};
 
+  // Read imaginary time data from "input.h5" file
+  auto input = h5::file(DATA_PATH + "/input.h5", 'r');
+  h5::read(input, "data", G_iw);
+  h5::read(input, "mesh", mesh);
+
+  // Build Nevanlinna factorization
   kernel.init(mesh, G_iw);
 
-  nda::array<std::complex<double>, 1> grid(N_omega);
-  int i = 0;
-  std::transform(grid.begin(), grid.end(), grid.begin(),
-                 [&](const std::complex<double> &) { return omega_min + (i++) * (omega_max - omega_min) / (N_omega - 1) + eta * 1i; });
-
-  auto G_omega = kernel.evaluate(grid);
+  // Perform analytical continuation onto real frequency axis
+  auto G_w = kernel.evaluate(grid);
 }
